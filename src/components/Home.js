666fbebce93fd2b4f1c8942c59cfea94b8a41a26
Bhushan1020 <bhushannarawade2003@gmail.com>
2024-05-30 10:57:13 +0530

    Added new changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className="home-container">
       <div className="center-block">
-        <h1>Make Yourself At Home <br/> In Our <span style={{ color: '#8d0034' }}>Guest House. </span></h1>
+        <p>Make Yourself At Home <br/> In Our <span style={{ color: '#8d0034' }}>Guest House. </span></p>
       </div>
     </div>
   );
